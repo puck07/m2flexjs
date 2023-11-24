12cf093c600cf89e4d932ed9b95d937b630b0f47
puck <36668@ma-web.nl>
2023-11-24 12:26:31 +0100

    puck

--- app.js
+++ app.js
@@ -1,15 +1,70 @@
 let canvas = document.getElementById("canvas");
 let g = canvas.getContext("2d");
 
-const gamestate_start=0;
-const gamestate_ingame=1;
-const gamestate_gameover=2;
+const gamesstate_start = 0;
+const gamesstate_ingame = 1;
+const gamesstate_gameover = 2;
 
-const ingamestate_start=0;
-const ingamestate_roll=1;
-const gamestate_end=0;
+const ingamestate_start = 0;
+const ingamestate_roll = 1;
+const ingamestate_end = 0;
 
-let boardPositionSize= 50;
-let pawnPostition = [];
-let boardPosition=[];
+let boardPositionSize = 50;
+let pawnPositions = [];
+let boardPositions = [];
 let playerAmountButtons = [];
+
+function createRect(x, y, w, h) {
+    let rectangle = {
+        x: x,
+        y: y,
+        x2: x + w,
+        y2: y + h,
+        w: w,
+        h: h,
+    };
+    return rectangle;
+}
+
+function clearCanvas() {
+    g.fillStyle = "lightslategray";
+    g.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function draw() {
+    clearCanvas();
+    for (let i = 0; i < boardPositions.length; i++) {
+        let pos = boardPositions[i];
+
+        g.fillStyle = "#004400";
+        g.fillRect(pos.x, pos.y, pos.h, pos.w);
+        g.fillStyle = "#FFFFFF";
+        g.fillText((i + 1) + "", pos.x, pos.y + 20);
+    }
+}
+
+function createBoardPositions() {
+    let x = 0;
+    let y = canvas.height - boardPositionSize;
+    let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+    for (let i = 0; i < path.length; i++) {
+
+        if (path[i] == 1)//gaan naar rechts
+        {
+            x += boardPositionSize;
+        }
+        else if (path[i] == 3)//gaan naar links
+        {
+            x -= boardPositionSize;
+        }
+        else if (path[i] == 0)//gaan hier naar boven
+        {
+            y -= boardPositionSize;
+        }
+        boardPositions.push(createRect(x, y, boardPositionSize, boardPositionSize));
+    }
+}
+
+createBoardPositions();
+draw();
