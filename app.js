8ff02f278641110a0b3374dd105e29e20efca7d3
puck <36668@ma-web.nl>
2023-11-24 11:53:32 +0100

    add

---
+++ app.js
@@ -0,0 +1,15 @@
+let canvas = document.getElementById("canvas");
+let g = canvas.getContext("2d");
+
+const gamestate_start=0;
+const gamestate_ingame=1;
+const gamestate_gameover=2;
+
+const ingamestate_start=0;
+const ingamestate_roll=1;
+const gamestate_end=0;
+
+let boardPositionSize= 50;
+let pawnPostition = [];
+let boardPosition=[];
+let playerAmountButtons = [];
